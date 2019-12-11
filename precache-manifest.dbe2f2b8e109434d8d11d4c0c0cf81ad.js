f97b9c0236d869978b229d7dc5ed08531adce187
Thomas GUILLIER <thomas.guillier@sogeti.com>
2019-12-11 12:34:22 +0100

    Updates

---
+++ precache-manifest.dbe2f2b8e109434d8d11d4c0c0cf81ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb117710beb24fa9cea5f6a1e65e1e4d",
+    "url": "/navette.react/index.html"
+  },
+  {
+    "revision": "4d707e1c577ab3bc3507",
+    "url": "/navette.react/static/css/main.7e1be9b5.chunk.css"
+  },
+  {
+    "revision": "ab32fa8c88d978cd8480",
+    "url": "/navette.react/static/js/2.edccbc4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/navette.react/static/js/2.edccbc4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d707e1c577ab3bc3507",
+    "url": "/navette.react/static/js/main.c534b3df.chunk.js"
+  },
+  {
+    "revision": "abd55baf14735f0b3fdb",
+    "url": "/navette.react/static/js/runtime-main.b5cdc5b8.js"
+  }
+]);
